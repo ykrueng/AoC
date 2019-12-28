bebdcc87ba49a515888198f8023d8a7281adaed5
Yasirah Krueng <yasirah.krueng@arundo.com>
2019-12-28 11:07:24 -0600

    solve day 1 part 2

--- day1/index.js
+++ day1/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const { getTotalFuelCount } = require("./helper");
+const { getTotalFuelCount, getTotalFinalFuelCount } = require("./helper");
 
 // read the input file
 const inputStr = fs.readFileSync(path.join(__dirname, "./input.txt"), "utf-8");
@@ -11,7 +11,11 @@ const inputArr = inputStr
   .map(e => parseInt(e));
 
 exports.dayOnePartOne = () => {
-  return `The sum of the fuel requirements for all of the modules on the spacecraft is: ${getTotalFuelCount(
-    inputArr
-  )}`;
+  const fuel = getTotalFuelCount(inputArr);
+  return `The sum of the fuel requirements for all of the modules on the spacecraft is: ${fuel}`;
+};
+
+exports.dayOnePartTwo = () => {
+  const fuel = getTotalFinalFuelCount(inputArr);
+  return `The sum of the fuel requirements for all of the modules on the spacecraft when also taking into account the mass of the added fuel is: ${fuel}`;
 };
