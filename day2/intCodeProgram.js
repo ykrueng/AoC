da8c7c593e9fde254e37011fd8614a5ee239a76f
Yasirah Krueng <yasirah.krueng@arundo.com>
2019-12-28 14:30:00 -0600

    refactor IntCodeProgram

--- day2/intCodeProgram.js
+++ day2/intCodeProgram.js
@@ -20,23 +20,18 @@ function IntCodeProgram(input, config) {
 }
 
 IntCodeProgram.prototype.run = function() {
+  const validInstruction = [1, 2, 99];
+
   while (this.pointer < this.codes.length) {
-    switch (this.codes[this.pointer]) {
-      case 99:
-        this.opCode99();
-        break;
-      case 1:
-        this.opCode1();
-        break;
-      case 2:
-        this.opCode2();
-        break;
-      default:
-        console.error(
-          `Encountered an invalid instruction: ${this.codes[this.pointer]}`
-        );
-        this.pointer = this.codes.length;
-        break;
+    const instruction = this.codes[this.pointer];
+
+    if (validInstruction.includes(instruction)) {
+      this[`opCode${instruction}`]();
+    } else {
+      console.error(
+        `Encountered an invalid instruction: ${this.codes[this.pointer]}`
+      );
+      this.pointer = this.codes.length;
     }
   }
 };
