99f6f34d251d9b6073d2f779b79491cd7d82f76d
Yasirah Krueng <yasirah.krueng@arundo.com>
2019-12-28 10:58:07 -0600

    add test for part two

--- day1/helper.test.js
+++ day1/helper.test.js
@@ -1,7 +1,12 @@
-const { getFuelCount, getTotalFuelCount } = require("./helper");
+const {
+  getFuelCount,
+  getTotalFuelCount,
+  getFinalFuelCount,
+  getTotalFinalFuelCount
+} = require("./helper");
 
 describe("Fuel required to launch a module", () => {
-  test("getModuleTotalFuel", () => {
+  test("getFuelCount", () => {
     expect(getFuelCount(0)).toEqual(0);
     expect(getFuelCount(12)).toEqual(2);
     expect(getFuelCount(14)).toEqual(2);
@@ -11,8 +16,25 @@ describe("Fuel required to launch a module", () => {
   });
 });
 
-describe("Fuel required to launch a module", () => {
-  test("getModuleTotalFuel", () => {
+describe("Total fuel required to launch modules", () => {
+  test("GetTotalFuelCount", () => {
     expect(getTotalFuelCount([0, 12, 14, 1969, 100756])).toEqual(34241);
+  });
+});
+
+describe("Final fuel required to launch a module", () => {
+  test("getFinalFuelCount", () => {
+    expect(getFinalFuelCount(0)).toEqual(0);
+    expect(getFinalFuelCount(12)).toEqual(2);
+    expect(getFinalFuelCount(14)).toEqual(2);
+    expect(getFinalFuelCount(1969)).toEqual(966);
+    expect(getFinalFuelCount(100756)).toEqual(50346);
+    expect(getFinalFuelCount("100756")).toEqual(NaN);
+  });
+});
+
+describe("Total final fuel required to launch modules", () => {
+  test("getTotalFinalFuelCount", () => {
+    expect(getTotalFinalFuelCount([0, 12, 14, 1969, 100756])).toEqual(51316);
   });
 });
