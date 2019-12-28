b723e01734640fcc3ab2953c3563118e3a8db61e
Yasirah Krueng <yasirah.krueng@arundo.com>
2019-12-28 10:33:20 -0600

    implement a function to get a total fuel

---
+++ day1/helper.js
@@ -0,0 +1,10 @@
+exports.getTotalFuel = mass => {
+  if (typeof mass !== "number") return NaN;
+  
+  const base = Math.floor(mass / 3);
+  if (base <= 2) {
+    return 0;
+  } else {
+    return base - 2;
+  }
+};
