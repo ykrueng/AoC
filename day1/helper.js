0ce5fd23005cd8695b691c01eaeb0819866e0038
Yasirah Krueng <yasirah.krueng@arundo.com>
2019-12-28 11:02:08 -0600

    implement functions to get modules final fuel count

--- day1/helper.js
+++ day1/helper.js
@@ -17,3 +17,22 @@ exports.getTotalFuelCount = modules => {
 
   return modules.reduce((acc, module) => acc + this.getFuelCount(module), 0);
 };
+
+exports.getFinalFuelCount = mass => {
+  if (typeof mass !== 'number') return NaN
+  if (mass <= 0) {
+    return 0
+  } else {
+    const fuel = this.getFuelCount(mass)
+    return fuel + this.getFinalFuelCount(fuel)
+  }
+}
+
+exports.getTotalFinalFuelCount = modules => {
+  if (modules.length <= 0) return 0;
+
+  const isInvalid = modules.some(module => typeof module !== "number");
+  if (isInvalid) return NaN;
+
+  return modules.reduce((acc, module) => acc + this.getFinalFuelCount(module), 0);
+};
