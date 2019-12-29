bab1f7e690817b99e45c78d58bd47e1efd40987a
Yasirah Krueng <yasirah.krueng@arundo.com>
2019-12-29 13:00:32 -0600

    add more test

--- day6/helper.test.js
+++ day6/helper.test.js
@@ -1,4 +1,4 @@
-const { orbitCountChecksums } = require("./helper");
+const { orbitCountChecksums, getMinTransferToSan } = require("./helper");
 
 const input = `COM)B
 B)C
@@ -12,11 +12,35 @@ E)J
 J)K
 K)L`;
 
+const sanInput = `
+COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN
+`
+
 const inputArr = input.trim().split("\n");
+const sanInputArr = sanInput.trim().split("\n");
 
 describe("The function to get orbit count checksums", () => {
   test("orbitCountChecksums", () => {
     const checksums = orbitCountChecksums(inputArr);
     expect(checksums).toEqual(42);
+  });
+});
+
+describe("The function to get orbit count from you to san", () => {
+  test("orbitCountChecksums", () => {
+    const min = getMinTransferToSan(sanInputArr);
+    expect(min).toEqual(4);
   });
 });
