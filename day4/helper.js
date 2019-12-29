a3cd02745fe4af0cebdb1e6ccaa079c185bf272d
Yasirah Krueng <yasirah.krueng@arundo.com>
2019-12-28 22:25:02 -0600

    implement strict get password count

--- day4/helper.js
+++ day4/helper.js
@@ -1,25 +1,38 @@
-exports.getPasswordCount = (loLimit, hiLimit) => {
+exports.getPasswordCount = (loLimit, hiLimit, strict = false) => {
   let count = 0;
 
   for (let i = loLimit; i <= hiLimit; i++) {
-    const hasDouble = this.validateHasDouble(i.toString());
+    const hasDouble = this.validateHasDouble(i.toString(), strict);
     const neverDecrease = this.validateNeverDecrease(i.toString());
     if (hasDouble && neverDecrease) count += 1;
   }
   return count;
 };
 
-exports.validateHasDouble = str => {
+exports.validateHasDouble = (str, strict = false) => {
   if (typeof str !== "string" && str.length < 2) return false;
 
-  let pointer = 1;
-  let current = str[0];
+  let headPointer = 1;
+  let tailPointer = 0;
+  let count = 1;
 
-  while (pointer < str.length) {
-    if (str[pointer] === current) return true;
+  while (headPointer < str.length) {
+    if (str[tailPointer] === str[headPointer]) {
+      count += 1;
+      if (!strict) return true;
+      if (count === 2) {
+        const lastIndex = headPointer === str.length - 1;
+        const nextEl = str[headPointer + 1];
 
-    current = str[pointer];
-    pointer += 1;
+        if (lastIndex || nextEl !== str[headPointer]) return true;
+      }
+      headPointer = headPointer + 1;
+      count += 1;
+    } else {
+      tailPointer = headPointer;
+      headPointer = headPointer + 1;
+      count = 1;
+    }
   }
 
   return false;
