710fb5709cede85a11525932f9e949c840af844a
Yasirah Krueng <yasirah.krueng@arundo.com>
2019-12-28 21:52:58 -0600

    solve day 4 part 1

---
+++ day4/index.js
@@ -0,0 +1,9 @@
+const { getInputArray } = require("../utility");
+const { getPasswordCount } = require("./helper");
+
+const str = getInputArray("day4/input.txt", "-");
+
+exports.dayFourPartOne = () => {
+  const passwordCount = getPasswordCount(str[0], str[1]);
+  return `The number of password within the range given in the puzzle input that meet the criteria is: ${passwordCount}`;
+};
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { dayOnePartOne, dayOnePartTwo } = require("./day1");
 const { dayTwoPartOne, dayTwoPartTwo } = require("./day2");
 const { dayThreePartOne, dayThreePartTwo } = require("./day3");
+const { dayFourPartOne } = require("./day4");
 
 function main() {
   console.log("Advent of Code 2019");
@@ -34,6 +35,11 @@ function main() {
 
   console.log("Day 3 part 2");
   console.log(dayThreePartTwo()); //13836
+  console.log("===================");
+  console.log("");
+
+  console.log("Day 4 part 1");
+  console.log(dayFourPartOne()); //1154
 }
 
 main();
