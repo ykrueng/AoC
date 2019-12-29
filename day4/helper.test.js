746606ab9f85e95bc9e8cf04b9779c552e25dc2a
Yasirah Krueng <yasirah.krueng@arundo.com>
2019-12-28 22:24:28 -0600

    add test

--- day4/helper.test.js
+++ day4/helper.test.js
@@ -5,10 +5,15 @@ const input2 = 223450;
 const input3 = 123789;
 
 describe("the validateHasDouble", () => {
-  test("validateHasDouble", () => {
+  it("should validate if input has double", () => {
     expect(validateHasDouble(input1.toString())).toEqual(true);
     expect(validateHasDouble(input2.toString())).toEqual(true);
     expect(validateHasDouble(input3.toString())).toEqual(false);
+  });
+  it("should validate if input has strict double", () => {
+    expect(validateHasDouble((112233).toString(), true)).toEqual(true);
+    expect(validateHasDouble((123444).toString(), true)).toEqual(false);
+    expect(validateHasDouble((111122).toString(), true)).toEqual(true);
   });
 });
 
@@ -17,5 +22,8 @@ describe("the validateNeverDecrease", () => {
     expect(validateNeverDecrease(input1.toString())).toEqual(true);
     expect(validateNeverDecrease(input2.toString())).toEqual(false);
     expect(validateNeverDecrease(input3.toString())).toEqual(true);
+    expect(validateNeverDecrease((112233).toString())).toEqual(true);
+    expect(validateNeverDecrease((123444).toString())).toEqual(true);
+    expect(validateNeverDecrease((111122).toString())).toEqual(true);
   });
 });
