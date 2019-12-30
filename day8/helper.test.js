eae466176f157e10de086c5ae4769d1594bb0041
Yasirah Krueng <yasirah.krueng@arundo.com>
2019-12-29 20:17:12 -0600

    add more test

--- day8/helper.test.js
+++ day8/helper.test.js
@@ -1,10 +1,18 @@
-const { decodeSpaceImage } = require("./helper");
+const { decodeSpaceImage, decodeMessage } = require("./helper");
 
 const input = "1203475487120047548712034054871203405487";
+const image = "0222112222120000";
 
 describe("A program to decode space image", () => {
   test("decodeSpaceImage", () => {
     const code = decodeSpaceImage(input, [5, 2]);
-    expect(code).toEqual(1)
+    expect(code).toEqual(1);
+  });
+});
+
+describe("A program to decode message", () => {
+  test("decodeMessage", () => {
+    const code = decodeMessage(image, [2, 2]);
+    expect(code).toEqual("01\n10");
   });
 });
